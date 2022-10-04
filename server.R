a1aeb3028ff593d62f2b37163bc50fe9f9ea1876
Karlduino <karlduino@gmail.com>
2022-10-04 17:48:22 -0500

    Pull out data construction code into helpers.R

--- server.R
+++ server.R
@@ -1,20 +1,8 @@
 library(shiny)
-library(googlesheets4)
 library(broman)
 
-sheet_file <-"https://docs.google.com/spreadsheets/d/1U458S6zutd1dDgCGKRRVxzDfe5JfD8_ZA-x3PFkwGUg"
-col_types <- "Tccnnn"
-
-if(!dir.exists("data")) dir.create("data")
-
 rds_file <- "data/co2data.rds"
-if(file.exists(rds_file)) {
-    co2data <- readRDS(rds_file)
-} else {
-    co2data <- read_sheet(sheet_file, col_types=col_types)
-    saveRDS(co2data, rds_file)
-}
-
+co2data <- readRDS(rds_file)
 sn <- c("all", names(sort(table(co2data$SerialNumber), decreasing=TRUE)))
 
 # Define server logic required to plot various variables against mpg
