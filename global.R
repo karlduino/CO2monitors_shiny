e369bbdcef487945f4cd3a100a0a04b4f19d90ef
Karlduino <karlduino@gmail.com>
2022-10-06 10:58:08 -0500

    global.R: snip off sessions of <= 5 min

--- global.R
+++ global.R
@@ -3,5 +3,9 @@
 rds_file <- "data/co2data.rds"
 co2data <- readRDS(rds_file)
 
+# snip off session of <= 5 min
+tab <- table(co2data$SessionID)
+co2data <- subset(co2data, SessionID %in% names(tab)[tab > 5])
+
 # drop-down options: "all" + observed devices
 sn <- c("all", names(sort(table(co2data$SerialNumber), decreasing=TRUE)))
