88b42e63f911f340b2371224633af1b4aed5ef8d
Karlduino <karlduino@gmail.com>
2022-10-06 19:41:25 -0500

    helpers.R: trim off bad data

--- helpers.R
+++ helpers.R
@@ -9,5 +9,10 @@ if(file.exists(rds_file)) {
     co2data <- readRDS(rds_file)
 } else {
     co2data <- read_sheet(sheet_file, col_types=col_types)
+
+    # trim off garbage (ULC11 with values > 1000)
+    co2data <- subset(co2data, SessionID != "543625635471476" |
+                               Timestamp > lubridate::ymd_hms("2022-10-06 12:16:59"))
+
     saveRDS(co2data, rds_file)
 }
