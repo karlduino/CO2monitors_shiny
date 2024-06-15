dec6783885f21ab859556e7eaf577bc9be9f694d
Karlduino <karlduino@gmail.com>
2024-06-15 07:35:42 -0500

    Split off first half of 2024-06

--- helpers.R
+++ helpers.R
@@ -20,6 +20,7 @@ sheet_files <- c(
     "https://docs.google.com/spreadsheets/d/1afAang4zj7f6E1upwX_r43EA1bB5f-eQjwh79telfmk",
     "https://docs.google.com/spreadsheets/d/1B9OJcQtCzOe6_ZXCiv8_NS6stFwJLnB_RXi4Xz1NJJc",
     "https://docs.google.com/spreadsheets/d/1tmw1rfvXwLiPHELCMds9gd21eFW5b2DueOKmGWoJyD4",
+    "https://docs.google.com/spreadsheets/d/1AemmJbPUnzIHKf2H4C3O9PFFq5AjUCgCfJN8WlPNbGY",
     "https://docs.google.com/spreadsheets/d/1breVedGAwdb744riqB4VYyxFD3PBnyrNcNsGgAqs8Xo"
   )
 col_types <- "Tccnnn"
