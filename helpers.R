1aaaeec0f1a87d2c819e3289498f8e5f4f53bead
Karlduino <karlduino@gmail.com>
2025-04-03 08:42:13 -0500

    Split off 2025-04a

--- helpers.R
+++ helpers.R
@@ -43,6 +43,7 @@ sheet_files <- c(
     "https://docs.google.com/spreadsheets/d/1YCAvX84fkE7OI_kLgphGy0vgkjjhEzuc2QButea_Hls",
     "https://docs.google.com/spreadsheets/d/1VnCLP-BhEXFeHIzJYRZxadwzCCeRsEUTWZRRq6RlfYM",
     "https://docs.google.com/spreadsheets/d/1-5l_H0RCqPStKny4QwGzE5j2pc5-db2dvLViUs8SDQE",
+    "https://docs.google.com/spreadsheets/d/1SsLKk6JiGpFaqX3CvyK1b0lCkE6G3d1ARy_9J8ic1s4",
     "https://docs.google.com/spreadsheets/d/1breVedGAwdb744riqB4VYyxFD3PBnyrNcNsGgAqs8Xo"
   )
 col_types <- "Tccnnn"
