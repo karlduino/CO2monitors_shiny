3755ddac1bda1913845d33f400c363251e44e968
Karlduino <karlduino@gmail.com>
2025-06-05 10:25:29 -0500

    Split off 2025-06a

--- helpers.R
+++ helpers.R
@@ -52,7 +52,8 @@ sheet_files <- c(
     "https://docs.google.com/spreadsheets/d/1wDQzegC4gWm3h0PZ_4F658T530AxxiIcbLsdhITJnUA",
     "https://docs.google.com/spreadsheets/d/1k6eL8sv1p1ztr7ChnH_e2wMod4PJtkbxtRjf2ChfAAs",
     "https://docs.google.com/spreadsheets/d/1XGppfkjYxOZvdIkw1_ssQUgTWP5cQgr25XGfd5ZFTxw",
-    "https://docs.google.com/spreadsheets/d/1breVedGAwdb744riqB4VYyxFD3PBnyrNcNsGgAqs8Xo"
+    "https://docs.google.com/spreadsheets/d/1LMr6GPGCQ8D-nPD6UOZIsl8N2TshE50fRwr7DgEuwiM",
+    "https://docs.google.com/spreadsheets/d/1KBesbsNsFpTeg1fiGlQn6vvlMqIk1nhUqQiwHzoCon4"
   )
 col_types <- "Tccnnn"
 
