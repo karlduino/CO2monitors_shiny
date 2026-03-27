8a911f6158e5950e9ce89e48aa419a38d1e92f8f
Karlduino <karlduino@gmail.com>
2026-03-27 08:44:14 -0500

    split off data 2026-03e

--- helpers.R
+++ helpers.R
@@ -101,6 +101,7 @@ sheet_files <- c(
     "https://docs.google.com/spreadsheets/d/129d1HLx-gVXdtsoX_dXUgfT8Ap27-RvEIH6_YSBeJyc",
     "https://docs.google.com/spreadsheets/d/1AYprPqx1qsdf6-ojWMzW6R-LBorU2bofK2go9OcdfP4",
     "https://docs.google.com/spreadsheets/d/1aGxAG6Uak6-sN6Cqxl4CRbeqYZJgJQIf2rcObE5xmY0",
+    "https://docs.google.com/spreadsheets/d/1rigHbMqLIn55QPZVtdK-DbsO9LZe2NBZsewXGsppXyY",
     "https://docs.google.com/spreadsheets/d/1KBesbsNsFpTeg1fiGlQn6vvlMqIk1nhUqQiwHzoCon4"
   )
 col_types <- "Tccnnn"
