eb3303abc3600a39635ca4a0ad0db1ecbee90fa1
Karlduino <karlduino@gmail.com>
2026-05-20 06:16:46 -0500

    split off data 2026-05d

--- helpers.R
+++ helpers.R
@@ -110,6 +110,7 @@ sheet_files <- c(
     "https://docs.google.com/spreadsheets/d/14rdfVRfXK00ekZilgpp92WH1AiNZ6WpFWMs5PwsIp78",
     "https://docs.google.com/spreadsheets/d/16pgEIprZ3K505oZaBjf6DFdd9mTF9jAGmGLVXc6TEzg",
     "https://docs.google.com/spreadsheets/d/1X6NVkC0_qKRbgiEZxAc6jphw5-2qb5K0eKVvvgb_qmo",
+    "https://docs.google.com/spreadsheets/d/1QufOf2kCO7k0SV4gPMYddKQ57hHIEX3dcf5UBO1-3XA",
     "https://docs.google.com/spreadsheets/d/1KBesbsNsFpTeg1fiGlQn6vvlMqIk1nhUqQiwHzoCon4"
   )
 col_types <- "Tccnnn"
